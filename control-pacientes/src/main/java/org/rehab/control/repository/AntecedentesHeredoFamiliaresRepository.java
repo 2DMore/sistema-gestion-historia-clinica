fc1fda47de18e82a957f4a315397fa71d6363f18
Daniel Moreno <A20216399@alumnos.uady.mx>
2023-07-29 22:10:08 -0600

    Se agrego la seccion de AntecedentesHeredoFamiliares

---
+++ control-pacientes/src/main/java/org/rehab/control/repository/AntecedentesHeredoFamiliaresRepository.java
@@ -0,0 +1,9 @@
+package org.rehab.control.repository;
+
+import org.rehab.control.entity.AntecedentesHeredoFamiliares;
+import org.rehab.control.entity.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AntecedentesHeredoFamiliaresRepository extends JpaRepository<AntecedentesHeredoFamiliares, Long>{
+	
+}
