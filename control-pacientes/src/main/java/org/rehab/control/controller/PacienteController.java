cf23f9d22f2a3db5bb17535b206501690bfda933
WilberthGamboa <wgcampos15@gmail.com>
2023-07-30 01:23:26 -0600

    init sistema CH

--- control-pacientes/src/main/java/org/rehab/control/controller/PacienteController.java
+++ control-pacientes/src/main/java/org/rehab/control/controller/PacienteController.java
@@ -1,29 +1,23 @@
 package org.rehab.control.controller;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.rehab.control.entity.Paciente;
 import org.rehab.control.service.PacienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
 
-@Controller
-@RequestMapping(value= "/paciente")
+@RestController
+@RequestMapping(value="/paciente")
 public class PacienteController {
 
 	@Autowired
@@ -42,8 +36,13 @@ public class PacienteController {
 		return pacientes;
 	}*/
 	
-	
-	@GetMapping("/")
+	/*
+	@GetMapping()
+	public String xd(){
+		return "hola";
+	}
+	 */
+	@GetMapping()
 	@ResponseBody
 	public ResponseEntity<?> getAllPacientes(){
 		try {
---
+++ control-pacientes/src/main/java/org/rehab/control/repository/SistemaCHRepository.java
@@ -0,0 +1,5 @@
+package org.rehab.control.repository;
+
+public class SistemaCHRepository {
+    
+}
