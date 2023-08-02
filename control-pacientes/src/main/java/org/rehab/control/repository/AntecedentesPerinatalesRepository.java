1a93dcb8825f23cb1795d6956af5b389395fcf25
Daniel Moreno <A20216399@alumnos.uady.mx>
2023-08-02 17:58:58 -0600

    Requests AP listas

---
+++ control-pacientes/src/main/java/org/rehab/control/repository/AntecedentesPerinatalesRepository.java
@@ -0,0 +1,8 @@
+package org.rehab.control.repository;
+
+import org.rehab.control.entity.AntecedentesPerinatales;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AntecedentesPerinatalesRepository extends JpaRepository<AntecedentesPerinatales,Long>{
+
+}
